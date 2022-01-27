614b986b1888198a8e63ca1c70cc77156b72918e
Matthias Wolf <freebsd@rheinwolf.de>
2022-01-27 16:44:25 +0100

    Fixed crash due to missing OSCrypt initialization

--- www/chromium/files/patch-chrome_browser_chrome__browser__main__linux.cc
+++ www/chromium/files/patch-chrome_browser_chrome__browser__main__linux.cc
@@ -1,4 +1,4 @@
---- chrome/browser/chrome_browser_main_linux.cc.orig	2021-12-31 00:57:22 UTC
+--- chrome/browser/chrome_browser_main_linux.cc.orig	2022-01-20 10:35:49 UTC
 +++ chrome/browser/chrome_browser_main_linux.cc
 @@ -30,7 +30,7 @@
  #include "chrome/installer/util/google_update_settings.h"
@@ -9,16 +9,22 @@
  #include "chrome/browser/dbus_memory_pressure_evaluator_linux.h"
  #endif
  
-@@ -53,7 +53,7 @@ ChromeBrowserMainPartsLinux::~ChromeBrowserMainPartsLi
- }
+@@ -54,12 +54,14 @@ ChromeBrowserMainPartsLinux::~ChromeBrowserMainPartsLi
  
  void ChromeBrowserMainPartsLinux::PreProfileInit() {
--#if !BUILDFLAG(IS_CHROMEOS_ASH)
-+#if !BUILDFLAG(IS_CHROMEOS_ASH) && !defined(OS_BSD)
+ #if !BUILDFLAG(IS_CHROMEOS_ASH)
++#if !defined(OS_BSD)
    // Needs to be called after we have chrome::DIR_USER_DATA and
    // g_browser_process.  This happens in PreCreateThreads.
    // base::GetLinuxDistro() will initialize its value if needed.
-@@ -82,14 +82,14 @@ void ChromeBrowserMainPartsLinux::PreProfileInit() {
+   base::ThreadPool::PostTask(
+       FROM_HERE, {base::MayBlock(), base::TaskPriority::BEST_EFFORT},
+       base::BindOnce(base::IgnoreResult(&base::GetLinuxDistro)));
++#endif
+ 
+   // Set up crypt config. This should be kept in sync with the OSCrypt parts of
+   // SystemNetworkContextManager::OnNetworkServiceCreated.
+@@ -82,14 +84,14 @@ void ChromeBrowserMainPartsLinux::PreProfileInit() {
  }
  
  void ChromeBrowserMainPartsLinux::PostCreateMainMessageLoop() {
@@ -35,7 +41,7 @@
  void ChromeBrowserMainPartsLinux::PostBrowserStart() {
    // static_cast is safe because this is the only implementation of
    // MemoryPressureMonitor.
-@@ -108,7 +108,7 @@ void ChromeBrowserMainPartsLinux::PostBrowserStart() {
+@@ -108,7 +110,7 @@ void ChromeBrowserMainPartsLinux::PostBrowserStart() {
  #endif
  
  void ChromeBrowserMainPartsLinux::PostDestroyThreads() {
